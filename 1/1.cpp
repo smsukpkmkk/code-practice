cec0128aac6d2d541d79b11542930e780fb5751a
Shan MingSheng <dncsms@fox.mail>
2026-03-24 02:10:00 +0800

    第八次尝试

---
+++ 1/1.cpp
@@ -0,0 +1,15 @@
+#include<algorithm>
+#include<cstdio>
+#include<iostream>
+using namespace std;
+int main()
+{
+ 
+    int a[5]={1,2,3,4,5};
+
+    for(int i=0;i<5;i++)
+        printf("%d \n",a[i]);
+    int n=5;
+    cout << n << endl;
+    return 0;
+}
