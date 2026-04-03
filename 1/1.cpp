78bbffd340017538c3a2c3ee2c496503cd49da6c
Shan MingSheng <dncsms@fox.mail>
2026-04-03 18:20:33 +0800

    4.3

--- 1/1.cpp
+++ 1/1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     int a[5]={1,2,3,4,5};
-    for(int i=0;i<5;i++)
+    for (int i=0;i<5;i++)
         printf("%d \n",a[i]);
     int n=5;
     cout << n << endl;
--- 2/2.cpp
+++ 2/2.cpp
@@ -1,13 +1,11 @@
-#include<vector>
-#include<cstdio>
-#include<iostream>
-using namespace std;   
-int main()
-{
-    vector<int> a={1,2,3,4,5};
-    for(int i=0;i<a.size();i++)
-        printf("%d \n",a[i]);
-    int n=a.size();
-    cout << n << endl;
-    return 0;
+#include <iostream>
+#include <thread>
+#include <future>
+
+int main() {
+    std::future<int> f = std::async([] {
+        return 42;
+    });
+
+    std::cout << f.get() << std::endl;
 }
